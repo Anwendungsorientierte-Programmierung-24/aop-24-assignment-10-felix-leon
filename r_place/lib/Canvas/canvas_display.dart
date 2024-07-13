3cb5343c6c02f1ffccedd4d5f1ab927293ca295c
FelixVogelsang <83448819+FelixVogelsang@users.noreply.github.com>
2024-07-13 20:51:49 +0200

    Implemented basic GridService funktionalities

--- r_place/lib/Canvas/canvas_display.dart
+++ r_place/lib/Canvas/canvas_display.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:r_place/Canvas/canvas_service.dart';
 
 class CanvasDisplay extends StatefulWidget {
   @override
@@ -6,15 +7,22 @@ class CanvasDisplay extends StatefulWidget {
 }
 
 class _CanvasDisplayState extends State <CanvasDisplay> {
+  late CanvasService cService;
+  late List <Container> grid;
+
+  @override 
+  initState(){
+    super.initState();
+    cService = new CanvasService();
+    grid = cService.getList();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
-      crossAxisCount: 10,
-      children:[
-        Container(
-          decoration: BoxDecoration( color: Colors.green),
-        )
-      ]
-    );
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 10),
+      itemBuilder: (context, index){
+        return grid[index];
+      });
   }
 }
--- r_place/lib/Canvas/canvas_service.dart
+++ r_place/lib/Canvas/canvas_service.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
 class CanvasService {
-  late List <Container> grid;
-  final int size = 10;
+  late List <Container> _grid;
+  final int _size = 10;
 
   CanvasService (){
-    
+    _createGrid();
+  }
+
+  _createGrid(){
+    for (int y = 0; y < _size; y++){
+      for (int x = 0; x < _size; x++){
+        _grid.add(Container(decoration: BoxDecoration(color: Colors.green)));
+      }
+    }
+  }
+
+  List <Container> getList (){
+    return _grid;
   }
 }
