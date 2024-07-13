9556ee1f24c0a6a9f8f87256c67e5d5931474478
MaurerLeon <161095831+MaurerLeon@users.noreply.github.com>
2024-07-13 23:34:04 +0200

    Add login_screen.dart which displays the login feature for the app
    Added login_screen.dart to enable logging in via user credentials (email and password). Added comments for code clarity. Refactored build.gradle.
    
    Details:
    - added login_screen.dart with basic UI design to enable logging in with user credentials
    - added textfields to enter the user credentials in login_screen.dart
    - added _login method wich uses the signIn method of auth_service.dart to login
    - added textbutton for future navigation to the sign up screen
    - added comments for code clarity in login_screen.dart

---
+++ r_place/lib/screens/login_screen.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+import 'package:r_place/services/auth_service.dart';
+
+// class represents the login screen
+class LoginScreen extends StatefulWidget {
+  // constants
+  static const _appTitle = 'R/Place App';
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+// state class
+class _LoginScreenState extends State<LoginScreen> {
+  // class variables
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  // Method to login with user credentials using the signIn method from
+  //AuthService class
+  Future<void> _login() async {
+    // Instance of authService by using the Provider
+    final authService = Provider.of<AuthService>(context, listen: false);
+    // Try-block which uses the signIn method of the AuthService class and the
+    // texts from the TextEditinControllers as input
+    try {
+      await authService.signIn(_emailController.text, _passwordController.text);
+      // Navigation zum Canvas hinzufügen
+    } catch (e) {
+      // If sign in doesnt work a Snackbar will be displayed showing the
+      // specific error
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(
+          e.toString(),
+        ),
+      ));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // SafeArea to display the statusbar
+    return SafeArea(
+      child: Scaffold(
+        // AppBar displays the title of the app
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 5, 33, 248),
+          centerTitle: true,
+          title: const Text(
+            LoginScreen._appTitle,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 25.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        // SingelChildScrollView to disable renderflex overflow problems
+        body: SingleChildScrollView(
+          // Center to center the column with the features in it
+          child: Center(
+            child: Column(
+              children: [
+                // Padding for better placement of the image
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                          width: 3.0,
+                          color: Colors.black,
+                        ),
+                        borderRadius: BorderRadius.circular(8.0)),
+                    // Image is displayed
+                    child: Image.asset('assets/login_screen.jpg'),
+                  ),
+                ),
+                // SizedBox with the TextField for the emailinput
+                SizedBox(
+                  // Mediaquery for better placement
+                  width: MediaQuery.of(context).size.width / 2,
+                  // Textfield uses _emailController to use that input
+                  child: TextField(
+                    controller: _emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    // styled by Inputdecoration
+                    decoration: const InputDecoration(
+                        hintText: 'Email',
+                        fillColor: Color.fromARGB(255, 5, 33, 248),
+                        focusColor: Color.fromARGB(255, 5, 33, 248),
+                        hoverColor: Color.fromARGB(255, 5, 33, 248),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Color.fromARGB(255, 5, 33, 248)))),
+                    cursorColor: const Color.fromARGB(255, 5, 33, 248),
+                    cursorErrorColor: const Color.fromARGB(255, 5, 33, 248),
+                  ),
+                ),
+                // SizedBox to create space between both TextFields
+                const SizedBox(
+                  height: 30.0,
+                ),
+                // SizedBox with the TextField for the passwordinput
+                SizedBox(
+                  // Mediaquery for better placement
+                  width: MediaQuery.of(context).size.width / 2,
+                  // Textfield uses _emailController to use that input
+                  child: TextField(
+                    controller: _passwordController,
+                    obscureText: true,
+                    // styled by Inputdecoration
+                    decoration: const InputDecoration(
+                        hintText: 'Passwort',
+                        fillColor: Color.fromARGB(255, 5, 33, 248),
+                        focusColor: Color.fromARGB(255, 5, 33, 248),
+                        hoverColor: Color.fromARGB(255, 5, 33, 248),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Color.fromARGB(255, 5, 33, 248)))),
+                    cursorColor: const Color.fromARGB(255, 5, 33, 248),
+                    cursorErrorColor: const Color.fromARGB(255, 5, 33, 248),
+                  ),
+                ),
+                // SizedBox to create some space
+                const SizedBox(
+                  height: 30.0,
+                ),
+                // ElevatedButton uses _login method to login in the app with
+                // the given input
+                ElevatedButton(
+                  onPressed: () {
+                    _login();
+                  },
+                  child: const Text('Login'),
+                ),
+                // SizedBox to create some space
+                const SizedBox(
+                  width: 30.0,
+                ),
+                // TextButton to navigate to the registerscreen
+                TextButton(
+                  // Navigation zur Registrierseite hinzufügen
+                  onPressed: () {},
+                  child: const Text('Create Account'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
