4cad53a23b30ae9e4aa7937411a152c93959ee84
FelixVogelsang <83448819+FelixVogelsang@users.noreply.github.com>
2024-07-14 21:20:17 +0200

    Changed Colorepalette button layout

--- r_place/lib/Canvas/Canvas_color_palette.dart
+++ r_place/lib/Canvas/Canvas_color_palette.dart
@@ -12,44 +12,47 @@ class ColorPalette extends StatefulWidget {
 }
 
 class _ColorPaletteState extends State<ColorPalette> {
-final List <ColorPaletteTile> _tiles = [
-  ColorPaletteTile(Colors.white),
-  ColorPaletteTile(Colors.red),
-  ColorPaletteTile(Colors.green),
-  ColorPaletteTile(Colors.blue),
-  ColorPaletteTile(Colors.yellow),
-  ColorPaletteTile(Colors.brown),
-  ColorPaletteTile(Colors.black),
-  ColorPaletteTile(Colors.orange),
-  ColorPaletteTile(Colors.pink),
-  ColorPaletteTile(Colors.purple),
-  ColorPaletteTile(Colors.lightBlue),
-  ColorPaletteTile(Colors.grey)
-];
+  final List<ColorPaletteTile> _tiles = [
+    ColorPaletteTile(Colors.white),
+    ColorPaletteTile(Colors.red),
+    ColorPaletteTile(Colors.green),
+    ColorPaletteTile(Colors.blue),
+    ColorPaletteTile(Colors.yellow),
+    ColorPaletteTile(Colors.brown),
+    ColorPaletteTile(Colors.black),
+    ColorPaletteTile(Colors.orange),
+    ColorPaletteTile(Colors.pink),
+    ColorPaletteTile(Colors.purple),
+    ColorPaletteTile(Colors.lightBlue),
+    ColorPaletteTile(Colors.grey)
+  ];
 
-@override
+  @override
   void initState() {
     super.initState();
     _fillList();
   }
 
   //filling the tile list with funtions
-  _fillList(){
+  _fillList() {
     setState(() {
-      for (int i = 0; i < _tiles.length; i++){
-      _tiles[i].setFunctions(widget._onColorChange);
-    }
+      for (int i = 0; i < _tiles.length; i++) {
+        _tiles[i].setFunctions(widget._onColorChange);
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+    return Container(
+      decoration: BoxDecoration(color: Colors.black),
+        child: GridView.builder(
+      gridDelegate:
+          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
       itemCount: _tiles.length,
       itemBuilder: (BuildContext context, int index) {
         return _tiles[index];
       },
-    );
+    ));
   }
 }
--- r_place/lib/Canvas/color_palette_tile.dart
+++ r_place/lib/Canvas/color_palette_tile.dart
@@ -3,26 +3,26 @@ import 'package:flutter/material.dart';
 class ColorPaletteTile extends StatefulWidget {
   late Color _color;
   Color _borderColor = Colors.white;
-  late void Function (bool) _changeColor;
-  late void Function (Color) _onColorChange;
+  late void Function(Color) _onColorChange;
 
-  ColorPaletteTile(Color color){
+  ColorPaletteTile(Color color) {
     _color = color;
   }
   @override
   State<StatefulWidget> createState() => _ColorPaletteTileState();
 
   //setting funktions for tiles
-  setFunctions(void Function (Color) onColorChange){
+  setFunctions(void Function(Color) onColorChange) {
     _onColorChange = onColorChange;
   }
 }
 
-class _ColorPaletteTileState extends State <ColorPaletteTile>{
-
+class _ColorPaletteTileState extends State<ColorPaletteTile> {
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
+    return Container(
+        padding: EdgeInsets.all(10),
+        child: ElevatedButton(
             style: ElevatedButton.styleFrom(
                 shape: BeveledRectangleBorder(),
                 side: BorderSide(width: 3, color: widget._borderColor),
@@ -30,6 +30,6 @@ class _ColorPaletteTileState extends State <ColorPaletteTile>{
             child: null,
             onPressed: () {
               widget._onColorChange(widget._color);
-            });
+            }));
   }
 }
