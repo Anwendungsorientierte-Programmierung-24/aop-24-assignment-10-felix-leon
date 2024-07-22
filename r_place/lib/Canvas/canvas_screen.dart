f43fea3fcebe5ed57d0663474aa93b6a430f3993
MaurerLeon <161095831+MaurerLeon@users.noreply.github.com>
2024-07-22 10:24:22 +0200

    Revert "Merge pull request #14 from Anwendungsorientierte-Programmierung-24/connection_service"
    This reverts commit d0f69089a50c751beafa05c9928cb3bc989b0c1e, reversing
    changes made to 08d8a31e76b881bd5c05470750a4c711b5fb9119.

--- r_place/lib/Canvas/canvas_screen.dart
+++ r_place/lib/Canvas/canvas_screen.dart
@@ -1,17 +1,13 @@
-import 'dart:async';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:r_place/Canvas/pixel.dart';
 import 'package:r_place/Canvas/pixel_service.dart';
 import 'package:r_place/screens/login_screen.dart';
 import 'package:r_place/services/auth_service.dart';
-import 'package:r_place/services/connection_service.dart';
 
 // Main widget for the Canvas screen.
 class CanvasScreen extends StatefulWidget {
-  // Constants
+  // constantss
   static const String _title = 'R/Place Canvas';
   const CanvasScreen({super.key});
 
@@ -19,89 +15,10 @@ class CanvasScreen extends StatefulWidget {
   _CanvasScreenState createState() => _CanvasScreenState();
 }
 
-// State class for CanvasScreen to manage state and build the UI
+// State class for CanvasScreen to manage state and build the UI.
 class _CanvasScreenState extends State<CanvasScreen> {
-  // Class variables
-  Color _selectedColor = Colors.white; // Default selected color
-  bool _isConnected = false;
-  bool _dialogVisible = false;
-  bool _isSnackBarVisible = false;
-
-  // Override of the initState method to check the connection status
-  @override
-  void initState() {
-    super.initState();
-    _checkInitialConnection();
-
-    // Subscribe to connectivity changes
-    final connectionService =
-        Provider.of<ConnectionService>(context, listen: false);
-    connectionService.status.listen((isConnected) {
-      setState(() {
-        _isConnected = isConnected;
-      });
-      // if there is no connection a dialog will be shown by using the
-      // _showDialog method
-      if (!_isConnected) {
-        _showErrorDialog();
-        // if the connection is restored a snackbar will be displayed
-      } else {
-        _showSnackBar();
-      }
-    });
-  }
-
-  // Method displays a snackbar with a message
-  void _showSnackBar() {
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(
-        backgroundColor: Colors.green,
-        content: Text(
-          'Internet connection restored',
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
-    );
-  }
-
-  // Method displays a Dialog if there is no connection with the internet
-  void _showErrorDialog() {
-    // Validates that the dialog wasnt displayed before
-    if (!_dialogVisible) {
-      _dialogVisible = true;
-      // ShowDialog displays the error message
-      showDialog(
-        context: context,
-        barrierDismissible: false,
-        builder: (context) => AlertDialog(
-          title: const Text(
-            'Connection problem',
-            style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
-            textAlign: TextAlign.center,
-          ),
-          content: const Text(
-            'No internet connection. Please check your connection.',
-            textAlign: TextAlign.center,
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                _dialogVisible = false;
-                Navigator.of(context).pop();
-              },
-              child: const Center(
-                child: Text(
-                  'OK',
-                  style: TextStyle(color: Color.fromARGB(255, 5, 33, 248)),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-          ],
-        ),
-      );
-    }
-  }
+  // class variables
+  Color _selectedColor = Colors.white; // Default selected color.
 
   // Navigates to the login screen.
   void _navigateToLoginScreen() {
@@ -113,9 +30,9 @@ class _CanvasScreenState extends State<CanvasScreen> {
   Future<void> _logout() async {
     final authService = Provider.of<AuthService>(context, listen: false);
     try {
-      // Sign out the user
+      // Sign out the user by using the signOut method of AuthService
       await authService.signOut();
-      // Navigate to the login screen
+      // Navigate to the login screen by using _navigateToLoginScreen
       _navigateToLoginScreen();
     } catch (e) {
       // Show any error message by a snackbar
@@ -124,29 +41,6 @@ class _CanvasScreenState extends State<CanvasScreen> {
           e.toString(),
         ),
       ));
-    }
-  }
-
-  // Checks the connection status of the phoen
-  Future<bool> checkConnection() async {
-    var connection = await (Connectivity().checkConnectivity());
-    // Connection with the wifi or flat is needed
-    if (connection.contains(ConnectivityResult.wifi) ||
-        connection.contains(ConnectivityResult.mobile)) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  // Method checks the initial connection by using the checkConnection method
-  void _checkInitialConnection() async {
-    bool isConnected = await checkConnection();
-    setState(() {
-      _isConnected = isConnected;
-    });
-    if (!_isConnected) {
-      _showErrorDialog();
     }
   }
 
@@ -161,8 +55,9 @@ class _CanvasScreenState extends State<CanvasScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final pixelService = Provider.of<PixelService>(context);
     // Access pixel service
+    final pixelService = Provider.of<PixelService>(context);
+    // SafeArea to display the statusbar
     return SafeArea(
       child: Scaffold(
         body: Container(
@@ -185,7 +80,8 @@ class _CanvasScreenState extends State<CanvasScreen> {
                 centerTitle: true,
                 backgroundColor: const Color.fromARGB(255, 5, 33, 248),
                 actions: [
-                  // Logout action when button is pressed by using the _logout method
+                  // Logout action when button is pressed by using the _logout
+                  // method
                   IconButton(
                     onPressed: _logout,
                     icon: const Icon(
@@ -198,113 +94,70 @@ class _CanvasScreenState extends State<CanvasScreen> {
               // SizedBox to create some space
               const SizedBox(height: 20.0),
               Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.white,
-                      width: 3.0,
-                    ),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.white,
+                    width: 3.0,
                   ),
-                  // Height of the grid
-                  height: MediaQuery.of(context).size.width,
-                  child: StreamBuilder<List<Pixel>>(
-                    // Stream to get pixel data
-                    stream: pixelService.getPixels(),
-                    builder: (context, snapshot) {
-                      // Loading indicator
-                      if (snapshot.hasError) {
-                        return Center(
-                          child: Text(
-                            'Error fetching pixels: ${snapshot.error}',
-                            style: const TextStyle(
-                                color: Colors.white, fontSize: 18.0),
-                            textAlign: TextAlign.center,
-                          ),
-                        );
-                      }
-
-                      if (!snapshot.hasData) {
-                        return const Center(child: CircularProgressIndicator());
-                      }
-                      // Access pixel data
-                      final pixels = snapshot.data!;
-                      return Container(
-                        // Background color for the grid
-                        color: Colors.white,
-                        child: GridView.builder(
-                          physics:
-                              // Disable scrolling
-                              const NeverScrollableScrollPhysics(),
-                          gridDelegate:
-                              const SliverGridDelegateWithFixedCrossAxisCount(
-                            // Number of columns in the grid
-                            crossAxisCount: 16,
-                            crossAxisSpacing: 0,
-                            mainAxisSpacing: 0,
-                          ),
-                          // Total number of pixels
-                          itemCount: 256,
-                          // Get pixel ID
-                          itemBuilder: (context, index) {
-                            final pixelId = index.toString();
-                            final pixel = pixels.firstWhere(
-                              (pixel) => pixel.id == pixelId,
-                              orElse: () => Pixel(
-                                id: pixelId,
-                                // Default color if pixel not found
-                                color: Colors.white,
-                              ),
-                            );
-                            return GestureDetector(
-                              onTap: () async {
-                                // if there is no connection a snackbar with an
-                                // error message will be displayed
-                                if (!_isConnected) {
-                                  if (!_isSnackBarVisible) {
-                                    _isSnackBarVisible = true;
-                                    ScaffoldMessenger.of(context)
-                                        .showSnackBar(
-                                          const SnackBar(
-                                            backgroundColor: Colors.red,
-                                            content: Text(
-                                              'No internet connection. Cannot update pixel.',
-                                              style: TextStyle(
-                                                  color: Colors.white),
-                                            ),
-                                            behavior: SnackBarBehavior.floating,
-                                          ),
-                                        )
-                                        .closed
-                                        .then((_) {
-                                      _isSnackBarVisible =
-                                          false; // Reset the flag when the Snackbar is closed
-                                    });
-                                  }
-                                  return;
-                                }
-                                try {
-                                  // Update pixel color on tap by using the
-                                  // updatePixel method from PixelService
-                                  await pixelService.updatePixel(
-                                      pixelId, _selectedColor);
-                                } catch (e) {
-                                  ScaffoldMessenger.of(context).showSnackBar(
-                                    SnackBar(
-                                      content: Text('Error updating pixel: $e'),
-                                    ),
-                                  );
-                                }
-                              },
-                              child: Container(
-                                // Set pixel color.
-                                color: pixel.color,
-                                margin: EdgeInsets.zero,
-                              ),
-                            );
-                          },
+                ),
+                // Height of the grid
+                height: MediaQuery.of(context).size.width,
+                child: StreamBuilder<List<Pixel>>(
+                  // Stream to get pixel data
+                  stream: pixelService.getPixels(),
+                  builder: (context, snapshot) {
+                    // Loading indicator
+                    if (!snapshot.hasData) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                    // Access pixel data
+                    final pixels = snapshot.data!;
+                    return Container(
+                      // Background color for the grid
+                      color: Colors.white,
+                      child: GridView.builder(
+                        physics:
+                            // Disable scrolling
+                            const NeverScrollableScrollPhysics(),
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          // Number of columns in the grid
+                          crossAxisCount: 16,
+                          crossAxisSpacing: 0,
+                          mainAxisSpacing: 0,
                         ),
-                      );
-                    },
-                  )),
+                        // Total number of pixels
+                        itemCount: 256,
+                        // Get pixel ID
+                        itemBuilder: (context, index) {
+                          final pixelId = index.toString();
+                          final pixel = pixels.firstWhere(
+                            (pixel) => pixel.id == pixelId,
+                            orElse: () => Pixel(
+                              id: pixelId,
+                              // Default color if pixel not found
+                              color: Colors.white,
+                            ),
+                          );
+                          return GestureDetector(
+                            onTap: () async {
+                              // Update pixel color on tap by using the
+                              // updatePixel method from PixelService
+                              await pixelService.updatePixel(
+                                  pixelId, _selectedColor);
+                            },
+                            child: Container(
+                              // Set pixel color.
+                              color: pixel.color,
+                              margin: EdgeInsets.zero,
+                            ),
+                          );
+                        },
+                      ),
+                    );
+                  },
+                ),
+              ),
               // SizedBox to create some space
               const SizedBox(height: 20.0),
               Padding(
